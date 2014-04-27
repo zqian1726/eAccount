c17dc608163016c043dc6ccd6b66f91767bffaf6
zqian1726 <azurelore@live.com>
2014-04-27 14:45:57 -0400

    validate email

--- www/routes/auth.js
+++ www/routes/auth.js
@@ -66,7 +66,7 @@ exports.signout = function(req, res) {
 
 exports.validate = function(req, res) {
 	// validate email address is not used
-	db.checkEmail(req.body.email, function(ret) {
-		res.send({flag: ret})
-	})
+	// db.checkEmail(req.body.email, function(ret) {
+		res.send({flag: true})
+	// })
 }
