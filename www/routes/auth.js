6f64a4603fbc7ca5879815298733a979b80ec310
zqian1726 <azurelore@live.com>
2014-04-27 22:37:19 -0400

    remove log

--- www/routes/auth.js
+++ www/routes/auth.js
@@ -1,32 +1,26 @@
 var db = require('./dao/dbOperation')
   , sha1 = require('sha1')
+  , default_cookie_time = 1000 * 60 * 60 * 2; // 2 hour
 
 /*
  * Middlewares: check authorization
  */
 exports.unauthorized = function (req, res, next) {
 	if (req.cookies.user && req.cookies.passport === sha1(req.cookies.user + "#This%is%eAcount%secret#")) {
-		console.log('unlog: logged as ' + req.cookies.user)
 		res.redirect('/home')
 	}
 	else {
-		console.log('unlog: unlogged')
 		next()
 	}
-	// next()
 }
 
 exports.authorized = function (req, res, next) {
 	if (req.cookies.user && req.cookies.passport === sha1(req.cookies.user + "#This%is%eAcount%secret#")) {
-		console.log('log: logged as ' + req.cookies.user)
 		next()
 	}
 	else {
-		console.log(req.session)
-		console.log('log: unlogged')
 		res.redirect('/')
 	}
-	// next()
 }
 
 /*
@@ -37,8 +31,8 @@ exports.signin = function(req, res) {
 	db.checkUser(req.body.email, req.body.password, function(ret) {
 		if (ret) {
 			// succeed
-			res.cookie('user', req.body.email, { maxAge: 1000 * 60 * 60 * 2 })
-			res.cookie('passport', sha1(req.body.email + "#This%is%eAcount%secret#"), { maxAge: 1000 * 60 * 60 * 2 })
+			res.cookie('user', req.body.email, { maxAge: default_cookie_time })
+			res.cookie('passport', sha1(req.body.email + "#This%is%eAcount%secret#"), { maxAge: default_cookie_time })
 			res.redirect('/home')
 		}
 		else {
@@ -53,8 +47,8 @@ exports.signup = function(req, res, next) {
 	db.registerUser(req.body.email, req.body.username, req.body.password, req.body.dob, req.body.gender,function(ret) {
 		if (ret == "success") {
 			// succeed
-			res.cookie('user', req.body.email, { maxAge: 1000 * 60 * 60 * 2 })
-			res.cookie('passport', sha1(req.body.email + "#This%is%eAcount%secret#"), { maxAge: 1000 * 60 * 60 * 2 })
+			res.cookie('user', req.body.email, { maxAge: default_cookie_time })
+			res.cookie('passport', sha1(req.body.email + "#This%is%eAcount%secret#"), { maxAge: default_cookie_time })
 			res.redirect('/home')
 		}
 		else {
@@ -66,7 +60,6 @@ exports.signup = function(req, res, next) {
 
 exports.signout = function(req, res) {
 	// log out
-	console.log("Log out!")
 	res.clearCookie('user');
 	res.clearCookie('passport');
 	res.redirect('/')
@@ -74,9 +67,7 @@ exports.signout = function(req, res) {
 
 exports.validate = function(req, res) {
 	// validate email address is not used
-		db.checkEmail(req.body.email, function(ret) {
-			res.type('json').send(ret)
-		})
-		// res.send({email: true})
-	// })
+	db.checkEmail(req.body.email, function(ret) {
+		res.type('json').send(ret)
+	})
 }
