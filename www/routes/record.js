6d226be320c05277695da1affb4a23a9e060aa5f
zqian1726 <azurelore@live.com>
2014-04-28 15:56:05 -0400

    record operations

--- www/routes/record.js
+++ www/routes/record.js
@@ -1,9 +1,29 @@
+var db = require('./dao/dbOperation')
+
 exports.list = function(req, res) {
 	// get record list
+	db.getRecords(req.cookies.user, function() {
+		if (ret == "error") {
+			console.log(req.cookies.user + "get records failed!")
+			res.send({error: true})
+		}
+		else {
+			res.send({error: false, recordList: ret})
+		}
+	})
 }
 
 exports.add = function(req, res) {
 	// add a record
+	db.addRecord(req.cookies.user, req.body.amount, req.body.category, req.body.desc, req.body.datetime, function(ret) {
+		if (ret == "success") {
+			res.send({error: true})
+		}
+		else {
+			console.log(req.cookies.user + "add record failed!")
+			res.send({error: false})
+		}
+	})
 }
 
 exports.update = function(req, res) {
