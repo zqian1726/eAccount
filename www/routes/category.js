213ac852ce17fe5374eaa439f8153deb963b4840
zqian1726 <azurelore@live.com>
2014-04-28 15:44:34 -0400

    category operations

--- www/routes/category.js
+++ www/routes/category.js
@@ -5,24 +5,45 @@ exports.list = function(req, res) {
 	db.getCategories(req.cookies.user, function(ret) {
 		if (ret == "error") {
 			console.log(req.cookies.user + "gets category failed!")
-			res.send({error: "error", categoryList: ret})
+			res.send({error: true, categoryList: ret})
 		}
 		else {
-			res.send({categoryList: ret})
+			res.send({error: false, categoryList: ret})
 		}
 	})
 }
 
 exports.add = function(req, res) {
 	// add a category
+	var item = req.body.name
+	db.addCategory(req.cookies.user, item, function(ret) {
+		if (ret == "success") {
+			res.send({error: true})
+		}
+		else {
+			console.log(req.cookies.user + "adds category failed!")
+			res.send({error: false})
+		}
+	})
 }
 
 exports.update = function(req, res) {
 	// update a category
+
 }
 
 exports.delete = function(req, res) {
 	// delete a category
+	var item = req.body.name
+	db.deleteCategory(req.cookies.user, item, function(ret) {
+		if (ret == "success") {
+			res.send({error: false})
+		}
+		else {
+			console.log(req.cookies.user + "adds category failed!")
+			res.send({error: true})
+		}
+	})
 }
 
 exports.index = function(req, res) {
