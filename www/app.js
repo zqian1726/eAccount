c2a5d0c4a951ae2cff5a8aa4668f02482b6f5c3a
zqian1726 <azurelore@live.com>
2014-05-03 15:47:11 -0400

    del

--- www/app.js
+++ www/app.js
@@ -63,7 +63,7 @@ app.post('/user/reset', auth.authorized, user.reset)
 // Record CRUD:
 app.post('/record/new', auth.authorized, record.add)
 app.put('/record/:rid', auth.authorized, record.update)
-app.del('/record/:rid', auth.authorized, record.delete)
+app.delete('/record/:rid', auth.authorized, record.delete)
 app.get('/record/list', auth.authorized, record.list)
 // Record page:
 app.get('/record', auth.authorized, record.index)
@@ -71,7 +71,7 @@ app.get('/record', auth.authorized, record.index)
 // Record CRUD:
 app.post('/category/new', auth.authorized, category.add)
 app.put('/category/:tag', auth.authorized, category.update)
-app.del('/category', auth.authorized, category.delete)
+app.delete('/category', auth.authorized, category.delete)
 app.get('/category/list', auth.authorized, category.list)
 // Record page:
 app.get('/category', auth.authorized, category.index)
