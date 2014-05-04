e3a23b3b2f742509e32bcb3b2b23b7c19b62ce60
zqian1726 <azurelore@live.com>
2014-05-04 18:01:54 -0400

    statistic

--- www/app.js
+++ www/app.js
@@ -78,9 +78,9 @@ app.get('/category', auth.authorized, category.index)
 
 // statistic page:
 app.get('/statistic', auth.authorized, statistic.index)
-app.get('/statistic/chart/bar', auth.authorized, statistic.bar)
-app.get('/statistic/chart/pan', auth.authorized, statistic.pan)
-app.get('/statistic/chart/line/:period', auth.authorized, statistic.line)
+app.get('/statistic/chart/bar/:date', auth.authorized, statistic.bar)
+app.get('/statistic/chart/pan/:date', auth.authorized, statistic.pan)
+app.get('/statistic/chart/line/:period/:date', auth.authorized, statistic.line)
 
 // map page:
 app.get('/map', auth.authorized, func.map)
--- www/routes/statistic.js
+++ www/routes/statistic.js
@@ -1,3 +1,5 @@
+var db = require('./dao/dbOperation')
+
 exports.index = function(req, res) {
 	// statistic index page
 	res.render('statistic', {
@@ -6,13 +8,27 @@ exports.index = function(req, res) {
 }
 
 exports.pan = function(req, res) {
+	var date = req.param.date
+	db.getRecords(req.cookies.user, function(ret) {
+		if (ret == "error") {
+			console.log(req.cookies.user + " get pan failed!")
+			res.send({error: true})
+		}
+		else {
 
+
+
+			res.send({error: false, balance: 1000, recordList: ret})
+		}
+	})
 }
 
 exports.bar = function(req, res) {
-
+	var date = req.param.date
 }
 
 exports.line = function(req, res) {
-
+	var period = req.param.period
+		, date = req.param.date
+	
 }
