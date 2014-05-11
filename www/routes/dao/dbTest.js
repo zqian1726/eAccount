73e93e9e07b3980863f8c5b03cc105ac28e38ed7
BurningEgg <yellowcup333@gmail.com>
2014-05-11 18:45:26 -0400

    data

--- www/routes/dao/dbTest.js
+++ www/routes/dao/dbTest.js
@@ -59,10 +59,10 @@ exports.addRecord = function(email,amount,category,desc,dateTime,callback){
 		
 	});
 }
-db.addRecord("1@1.com",25,"food","test","2010-12-01 12:15",function(result){console.log(result)});
-db.addRecord("1@1.com",25,"food","test","2012-12-01 12:15",function(result){console.log(result)});
-db.addRecord("1@1.com",25,"food","test","2013-12-01 12:15",function(result){console.log(result)});
-db.addRecord("1@1.com",25,"food","test","2009-12-01 12:15",function(result){console.log(result)});
+db.addRecord("1@1.com",25,"food","test","2010-01-01 12:15",function(result){console.log(result)});
+db.addRecord("1@1.com",25,"food","test","2012-01-01 12:15",function(result){console.log(result)});
+db.addRecord("1@1.com",25,"food","test","2013-01-01 12:15",function(result){console.log(result)});
+db.addRecord("1@1.com",25,"food","test","2014-01-01 12:15",function(result){console.log(result)});
 
 */
 
@@ -73,5 +73,40 @@ var db = require('./dbOperation');
 //db.editCategory('1@1.com',[{name:'food', line: 40},{name: 'shopping', line: 50}],function(result){console.log(result)})
 //db.deleteRecord('1@1.com','536697a4075150101b540de5',function(result){console.log(JSON.stringify(result))});
 //db.getRecords('1@1.com',function(result){console.log(JSON.stringify(result))});
-db.getRecordsRange('qwe8@qwe.com',"2014-05-00 00:00",function(result){console.log(JSON.stringify(result))}); 
+//db.getRecordsRange('qwe8@qwe.com',"2014-05-00 00:00",function(result){console.log(JSON.stringify(result))}); 
 //db.deleteRecord("1@1.com","536c34d2ce0df2fc281a9d23",25,function(result){console.log(result)});
+db.addRecord("test@test.com",-25,"food","for food","2014-01-11 12:11",function(result){console.log(result)});
+db.addRecord("test@test.com",-825,"shopping","for shopping","2014-01-11 05:11",function(result){console.log(result)});
+db.addRecord("test@test.com",-35,"other","for other","2014-01-11 06:11",function(result){console.log(result)});
+db.addRecord("test@test.com",-925,"rent","for rent","2014-01-11 13:11",function(result){console.log(result)});
+db.addRecord("test@test.com",5225,"income","for income","2014-01-11 14:11",function(result){console.log(result)});
+
+db.addRecord("test@test.com",-15,"food","for food","2014-02-11 12:11",function(result){console.log(result)});
+db.addRecord("test@test.com",-725,"shopping","for shopping","2014-02-11 05:11",function(result){console.log(result)});
+db.addRecord("test@test.com",-25,"other","for other","2014-02-11 06:11",function(result){console.log(result)});
+db.addRecord("test@test.com",-825,"rent","for rent","2014-02-11 13:11",function(result){console.log(result)});
+db.addRecord("test@test.com",4225,"income","for income","2014-02-11 14:11",function(result){console.log(result)});
+
+db.addRecord("test@test.com",-05,"food","for food","2014-03-11 12:11",function(result){console.log(result)});
+db.addRecord("test@test.com",-625,"shopping","for shopping","2014-03-11 05:11",function(result){console.log(result)});
+db.addRecord("test@test.com",-15,"other","for other","2014-03-11 06:11",function(result){console.log(result)});
+db.addRecord("test@test.com",-125,"rent","for rent","2014-03-11 13:11",function(result){console.log(result)});
+db.addRecord("test@test.com",2252,"income","for income","2014-03-11 14:11",function(result){console.log(result)});
+
+db.addRecord("test@test.com",-253,"food","for food","2014-04-11 12:11",function(result){console.log(result)});
+db.addRecord("test@test.com",-251,"shopping","for shopping","2014-04-11 05:11",function(result){console.log(result)});
+db.addRecord("test@test.com",-31,"other","for other","2014-04-11 06:11",function(result){console.log(result)});
+db.addRecord("test@test.com",-225,"rent","for rent","2014-04-11 13:11",function(result){console.log(result)});
+db.addRecord("test@test.com",15225,"income","for income","2014-04-11 14:11",function(result){console.log(result)});
+
+db.addRecord("test@test.com",-15,"food","for food","2014-05-11 12:11",function(result){console.log(result)});
+db.addRecord("test@test.com",-825,"shopping","for shopping","2014-05-11 05:11",function(result){console.log(result)});
+db.addRecord("test@test.com",-33,"other","for other","2014-05-11 06:11",function(result){console.log(result)});
+db.addRecord("test@test.com",-321,"rent","for rent","2014-05-11 13:11",function(result){console.log(result)});
+db.addRecord("test@test.com",4525,"income","for income","2014-05-11 14:11",function(result){console.log(result)});
+
+db.addRecord("test@test.com",-55,"food","for food","2014-01-14 12:11",function(result){console.log(result)});
+db.addRecord("test@test.com",-822,"shopping","for shopping","2014-01-14 05:11",function(result){console.log(result)});
+db.addRecord("test@test.com",-33,"other","for other","2014-01-14 06:11",function(result){console.log(result)});
+db.addRecord("test@test.com",-921,"rent","for rent","2014-01-14 13:11",function(result){console.log(result)});
+db.addRecord("test@test.com",4225,"income","for income","2014-01-14 14:11",function(result){console.log(result)});
